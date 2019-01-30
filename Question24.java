78edd7bf256d74158cefd958618c71d430e4b1e5
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-01-29 19:42:37 -0800

    Create Question24.java

---
+++ Question24.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/* program to declare class Rectangle with data members
+	length and breadth member functions Input, Output
+	and CalcArea*/
+
+class Rectangle {
+	float length = 10, breadth = 5;
+	void Input () {
+		Scanner input = new Scanner (System.in);
+		System.out.print ("Enter length: ");
+		length = input.nextFloat ();
+		System.out.print ("Enter breadth: ");
+		breadth = input.nextFloat ();
+	}
+	void Output () {
+		System.out.println ("Length of rectangle: " + length);
+		System.out.println ("Breadth of rectangle: " + breadth);
+		float area = CalcArea ();
+		System.out.println ("Area of rectangle: " + area);
+	}
+	float CalcArea () {
+		return (length * breadth);
+	}
+}
+
+class Question24 {
+	public static void main(String args[]) {
+		Rectangle rect = new Rectangle ();
+		rect.Input ();
+		rect.Output ();
+	}
+}
