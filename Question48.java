d7c30144af5ed2f8cd20f570e7c2332c1020cbf2
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-19 21:40:12 -0800

    Update Question48.java

--- Question48.java
+++ Question48.java
@@ -1,54 +1,38 @@
-class A extends Thread
-{
-	public void run()
-	{
-		
-		try
-		{
-			for(int i=1;i<=5;i++){
-
-
-			System.out.println(5*i);
-		
-			Thread.sleep(500);
+class A extends Thread {
+	public void run () {
+		try {
+			for(int i=1;i<=5;i++) {
+				System.out.println (5 * i);
+				Thread.sleep(500);
+			}
 		}
-	}
-		catch(InterruptedException e )
-		{
-
+		catch(InterruptedException e) {
+			System.out.println ("Caught!");
 		}
 	}
 }
 
-class B extends Thread
-{
-	public void run()
-	{
-		for(int j=1;j<=4;j++)
-		{
-			System.out.println("j="+j);
+class B extends Thread {
+	public void run () {
+		for(int j = 1; j <= 4; j++) {
+			System.out.println ("j = " + j);
 		}
 	}
 }
 
-class C extends Thread
-{
-	public void run()
-	{
-		for(int k=1;k<=5;k++)
-		{
-			System.out.println("k="+k);
+class C extends Thread {
+	public void run () {
+		for(int k=1;k<=5;k++) {
+			System.out.println ("k = " + k);
 		}
 	}
 }
 
-class Question48
-{
-	public static void main(String[] args)  
-	{
-	 	A obj1=new A();
-	 	B obj2=new B();
-	 	C obj3=new C();
+class Question48 {
+	public static void main(String[] args) {
+	 	A obj1 = new A();
+	 	B obj2 = new B();
+	 	C obj3 = new C();
 	 	obj1.start();
 	 	obj2.start();
 	 	obj3.start();	
