91206c9d86d5de0ee83446680cd1cf73ee05987e
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-20 19:41:39 +0530

    Update Question47.java

--- Question47.java
+++ Question47.java
@@ -1,15 +1,15 @@
-//  program to demonstrate creation of a single child thread.
+//  program to demonstrate creation of a single child thread
 
 class A extends Thread {
 	public void run () {
 		try {
-			for ( int i = 1; i <= 5; i++ ) {
-				System.out.println(5*i);
-				Thread.sleep(500);
+			for ( int i = 1; i <= 5; i++) {
+				System.out.println (5 * i);
+				Thread.sleep (500);
 			}
 		}
-		catch ( InterruptedException e ) {
-			System.out.println("Caught");
+		catch (InterruptedException e) {
+			System.out.println ("Caught!");
 		}
 	}
 }
