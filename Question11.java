f557a0683badfa7e1b0f6823c320efca031f20fd
Prajeshpuri <prajeshpuri@gmail.com>
2019-02-20 17:54:22 +0530

    Update Question11.java

--- Question11.java
+++ Question11.java
@@ -4,7 +4,7 @@ import static java.lang.Math.pow;
 // https://en.wikipedia.org/wiki/Narcissistic_number
 
 class Question11 {
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		for (int i = 1; i < 1000; i++) {
 			int digit_count = 0, sum = 0, digit = 0, num;
 			num = i;
