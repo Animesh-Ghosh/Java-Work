2d84520b8dc25ff1ea1fcc5f56a5c6dbe5bcda5f
Prajeshpuri <prajeshpuri@gmail.com>
2019-02-20 18:03:10 +0530

    updated

--- Question18.java
+++ Question18.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // program to compute factorial of input
 
 class Question18 {
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		Scanner input = new Scanner (System.in);
 		int n, x , factorial = 1;
 		System.out.print ("Enter a number: ");
