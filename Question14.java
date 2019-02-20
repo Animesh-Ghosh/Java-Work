03a40393de8a9ba6a14795d7a532697f57ce9d1e
Prajeshpuri <prajeshpuri@gmail.com>
2019-02-20 18:01:06 +0530

    Update Question14.java

--- Question14.java
+++ Question14.java
@@ -1,7 +1,7 @@
 // program to demonstrate use of break
 
 class Question14 {
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		for (int i = 0; i < 10; i++) {
 			if (i > 5) {
 				System.out.println ("Breaking out of the loop.");
