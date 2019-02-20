49533ebd5471f28dad4c1fe8c7b914e3fa5cb52b
Prajeshpuri <prajeshpuri@gmail.com>
2019-02-20 17:35:10 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Input {
-	static public void main (String args[]) {
+	static public void main (String[] args) {
 		Scanner input = new Scanner (System.in);
 		System.out.print ("Enter your name: ");
 		String name = input.nextLine ();
