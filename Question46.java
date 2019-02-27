26d0104f5d4f3e52811c827a55e04310c42b9d53
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-27 19:48:28 +0530

    Update Question46.java

--- Question46.java
+++ Question46.java
@@ -1,15 +1,12 @@
- import java.lang.Thread;
  //  program to demonstrate a single-threaded process
 
-class Question46
-{
-	public static void main(String[] args) 
-	{
-		Thread t=Thread.currentThread();
-		System.out.println("current thread is "+ t);
-		System.out.println("the name of thread is "+ t.getName());
-		t.setName("prajesh");
-		System.out.println("current thread is "+ t);
-		System.out.println("the name of thread is "+ t.getName());
+class Question46 {
+	public static void main(String[] args) {
+		Thread t = Thread.currentThread ();
+		System.out.println ("Current thread is " + t);
+		System.out.println ("The name of thread is " + t.getName ());
+		t.setName ("Test");
+		System.out.println ("Current thread is " + t);
+		System.out.println ("The name of thread is " + t.getName ());
 	}
 }
