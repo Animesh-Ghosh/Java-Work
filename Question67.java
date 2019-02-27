f120165416706c68289f45e0ad8afc72300aa149
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-27 22:36:44 +0530

    Update files
    Updating applet files.

--- Question67.java
+++ Question67.java
@@ -1,17 +1,18 @@
 import java.awt.*;
 import java.applet.*;
+
+// program to create an Applet to calculate the sum of two numbers
+
 /* <applet code="Question67" width=300 height=80> 
 	</applet>
 */
 
-public class Question67 extends Applet
-{
-	public void paint(Graphics g){
-		int x=5;
-		int y=3;
-		int z=x+y;
-		String s="sum of x and y is "+ String.valueOf(z);
-		g.drawString(s,10, 10);	
-	}
-	 
+public class Question67 extends Applet {
+	public void paint (Graphics g) {
+		int x = 5;
+		int y = 3;
+		int z = x + y;
+		String s = "Sum of x and y is " + String.valueOf(z);
+		g.drawString (s, 10, 10);
+	} 
 }
