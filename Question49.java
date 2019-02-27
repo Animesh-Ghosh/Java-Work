88eab483c410f4f18ba274b68be9580c2bb41169
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-02-27 20:08:19 +0530

    Update Question49.java

--- Question49.java
+++ Question49.java
@@ -1,18 +1,18 @@
 // program to demonstrate use of getId () function
 
 public class Question49 implements Runnable {
-   Thread t;
-   Question49() {
-      t = new Thread(this, "Admin Thread");
-      t.setPriority(1);
-      System.out.println("thread  = " + t);
-      t.start();
-   }
-   public void run() {
-      System.out.println("Name = " + t.getName());
-      System.out.print("Id = " + t.getId());
-   }
-   public static void main(String[] args) {
-      new Question49();
-   }
+	Thread t;
+	Question49 () {
+		t = new Thread (this, "Admin Thread");
+		t.setPriority (1);
+		System.out.println ("Thread  = " + t);
+                t.start();
+        }
+        public void run () {
+                System.out.println ("Name = " + t.getName ());
+                System.out.print ("Id = " + t.getId ());
+        }
+        public static void main(String[] args) {
+                new Question49 ();
+        }
 }
