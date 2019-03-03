90afe6b59af1ebfabb317055eddba60ff1d43650
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-03-03 20:45:41 +0530

    Update Question64.java

--- Question64.java
+++ Question64.java
@@ -1,19 +1,19 @@
 import java.awt.*;
 import java.applet.*;
-/*<applet code="Question64" width=300 height=50>
-<param name="yourname" value="jeff">
+
+// program to create an Applet and use param tag
+
+/* <applet code="Question64" width=300 height=50>
+<param name="yourname" value="muh name jeff!">
 </applet> */
-public class Question64 extends Applet
-{
+
+public class Question64 extends Applet {
 	String name;
-	public void init()
-	{
+	public void init () {
 		name = getParameter("yourname");
-		if(name == null)
-			name = "Not Found";
+		if (name == null) name = "Not Found";
 	}
-	public void paint(Graphics g)
-	{
-		g.drawString("Your name : " + name, 0,10);
+	public void paint (Graphics g) {
+		g.drawString("Your name: " + name, 0, 10);
 	}
 }
