3a1dc443542b4c1bd2dffe5cf78a1c8e7fedf35d
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-03-03 20:52:02 +0530

    Update Question65.java

--- Question65.java
+++ Question65.java
@@ -1,23 +1,23 @@
 import java.awt.*;
 import java.applet.*;
-/*<applet code="Question65" width=300 height=50>
+
+//  program to create an Applet and use setBackground () and setForeground ()
+
+/* <applet code="Question65" width=300 height=50>
 </applet> */
-public class Question65 extends Applet
-{
+
+public class Question65 extends Applet {
 	String msg;
-	public void init()
-	{
-		setBackground(Color.magenta);
-		setForeground(Color.white);
-		msg = " inside init() ";
+	public void init () {
+		setBackground (Color.magenta);
+		setForeground (Color.white);
+		msg = "Inside init() ";
 	}
-	public void start()
-	{
-		msg += " Inside start() ";
+	public void start() {
+		msg += "Inside start() ";
 	}
-	public void paint(Graphics g)
-	{
-		msg += " Inside paint() ";
-		g.drawString(msg,10,30);
+	public void paint(Graphics g) {
+		msg += "Inside paint() ";
+		g.drawString(msg, 10, 30);
 	}
 }
