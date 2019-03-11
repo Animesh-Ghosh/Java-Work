78aac73d9beb738566506e9c750dac1c09120a27
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-03-11 19:36:57 +0530

    Update Question74.java

--- Question74.java
+++ Question74.java
@@ -1,18 +1,21 @@
 import java.awt.*;
 import java.applet.*;
-//Program to create Labels
+import java.awt.event.*;
 
-/*
-<applet code="Question74" width=300 height=200>
-</applet>*/
-public class Question74 extends Applet{
-public void init()
-{
-  Label one=new Label("One");
-  Label two=new Label("Two");
-  Label three = new Label("Three");
-  add(one);
-  add(two);
-  add(three);
-}
+// program to demonstrate working of Label control
+
+/* <applet code="Question74" height=100 width=200>
+	</applet>
+*/
+
+public class Question74 extends Applet {
+	public void init () {
+		Label one = new Label ("One");
+  		Label two = new Label ("Two");
+  		Label three = new Label ("Three");
+  		// adding labels to applet window
+  		add (one);
+  		add (two);
+  		add (three);
+  	}
 }
