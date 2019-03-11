74eda58db4422cd5fda7877827ce1e2ee5d78314
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-03-11 19:36:24 +0530

    Update Question76.java

--- Question76.java
+++ Question76.java
@@ -1,45 +1,38 @@
 import java.awt.*;
-import java.awt.event.*;
 import java.applet.*;
-//Program to create CheckBoxes
+import java.awt.event.*;
 
-/*
-<applet code="Question76" width=200 height=150>
-</applet>
+// program to demonstrate working of CheckBox control
+
+/* <applet code="Question76" height=200 width=300>
+	</applet>
 */
-public class Question76 extends Applet implements ItemListener
-{ 
-  String msg="",msgwin98="",msgwinNT="",msgsolaris="";
-  Checkbox Win98,winNT,solaris;
-  
-  public void init()
-  {
-    Win98= new Checkbox("Windows98");
-    winNT= new Checkbox("Windows NT");
-    solaris = new Checkbox("Solaris");
-   
-    add(Win98);
-    add(winNT);
-    add(solaris);
-    
-    Win98.addItemListener(this);
-    solaris.addItemListener(this);
-    winNT.addItemListener(this);
-  }
-  public void itemStateChanged(ItemEvent ie)
-  {
-        msgwin98="Windows 98 "+Win98.getState();
-        msgwinNT="Windows NT "+winNT.getState();
-        msgsolaris="Solaris  "+solaris.getState();
-        repaint();
-  }
-  public void paint(Graphics g)
-  {
-    msg="Current state: ";
-    g.drawString(msg,6,100);
-    g.drawString(msgwinNT,6,120);
-    g.drawString(msgwin98,6,140);
-    g.drawString(msgsolaris,6,160);
 
-  }
+public class Question76 extends Applet implements ItemListener {
+	String msg, msgWin98, msgWinNT, msgSolaris;
+	Checkbox Win98, WinNT, Solaris;
+	public void init () {
+		Win98 = new Checkbox ("Windows98");
+		WinNT = new Checkbox ("Windows NT");
+		Solaris = new Checkbox ("Solaris");
+		add (Win98);
+		add (WinNT);
+		add (Solaris);
+		Win98.addItemListener (this);
+		WinNT.addItemListener (this);
+		Solaris.addItemListener (this);
+	}
+	public void itemStateChanged (ItemEvent ie) {
+		repaint ();
+	}
+	public void paint (Graphics g) {
+		msg = "Current state: ";
+		msgWin98 = "Windows 98: " + Win98.getState ();
+		msgWinNT = "Windows NT: " + WinNT.getState ();
+		msgSolaris = "Solaris: " + Solaris.getState ();
+		g.drawString (msg, 6, 100);
+		g.drawString (msgWin98, 6, 120);
+		g.drawString (msgWinNT, 6, 140);
+		g.drawString (msgSolaris, 6, 160);
+	}
 }
