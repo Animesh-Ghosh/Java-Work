8ca064aa2c40b4ac288c40b56dfb5805a5b546bb
MaDDogx <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-04-07 18:28:51 +0530

    Update Question49.java
    For some reason tab spaces were 8. Fixed it.

--- Question49.java
+++ Question49.java
@@ -1,19 +1,19 @@
 // program to demonstrate use of getId () function
 
 class Demo extends Thread {
-        public void run () {
-                System.out.println ("Name is " + this.getName () + ".");
-                System.out.println ("Thread ID: " + this.currentThread ().getId ());
-                for (int i = 3; i > 0; i--) 
-                        System.out.println (i);
-        }
+	public void run () {
+		System.out.println ("Name is " + this.getName () + ".");
+		System.out.println ("Thread ID: " + this.currentThread ().getId ());
+		for (int i = 3; i > 0; i--)
+			System.out.println (i);
+	}
 }
 
-public class Question49 {
-        public static void main(String[] args) {
-                for (int i = 0; i < 3; i++) {
-                        Demo obj = new Demo ();
-                        obj.start ();
-                }
-        }
+class Question49 {
+	public static void main (String args[]) {
+		for (int i = 0; i < 3; i++) {
+			Demo obj = new Demo ();
+			obj.start ();
+		}
+	}
 }
